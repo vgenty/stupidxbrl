c3ca18bb7ffcd3620e999431ae28b4c9837deb4c
Victor Genty <vgenty@nevis.columbia.edu>
2015-10-20 19:58:58 -0400

    stupidxbrl supports multiple views, connecting_play.ipynb gives example of how to take webscrape R*.html from report, get the values, then connect output of stupidxbrl. Next steps is to figure out how to handle these contexts. Some are instant, some are period. We also need inference function to infer data point for period on 10-K. I learned a lot today

--- stupidxbrl.py
+++ stupidxbrl.py
@@ -1,4 +1,5 @@
 import re
+import collections
 
 import pandas
 import bs4
@@ -6,20 +7,24 @@ import bs4
 class stupidXBRL():
     def __init__(self,filename):
 
-        assert isinstance(filename,str)
+        #assert isinstance(filename,str)
         
         self._filename = filename;
 
         self._idsearch       = re.compile('ID_[0-9]+')
         self._txtblocksearch = re.compile('TextBlock$')
         try: 
-            self._xbrl     = bs4.BeautifulSoup(open(filename),"lxml-xml")
+            #self._xbrl     = bs4.BeautifulSoup(open(filename),"lxml-xml")
+            self._xbrl     = bs4.BeautifulSoup(filename,"lxml-xml")
         except IOError:
             print '%s does not exist' % filename
             raise;
 
 
     def Build(self):
+        
+        nested_dict = lambda: collections.defaultdict(nested_dict)
+        
         ##########################
         #Build the xbrl "contexts" - date ranges for reported fields
         #
@@ -33,35 +38,37 @@ class stupidXBRL():
 
             if period.startDate is not None:
                 self.contexts[context.attrs['id']] = {'period' : {'startDate' : period.startDate.contents[0], 'endDate' : period.endDate.contents[0] },
-                                                 'type'   : 'range'}
+                                                      'type'   : 'range'}
             if period.instant is not None:
                 self.contexts[context.attrs['id']] = {'period' : {'Date' : period.instant.contents[0] },
-                                                 'type'   : 'instant'}
+                                                      'type'   : 'instant'}
     
         ##########################
         #Build the xbrl "dei" - document data
         #
-        self.document = {}
-        for dei in self._xbrl.find_all(lambda tag : tag.prefix == 'dei' and self._idsearch.search(tag.attrs['id']) is not None):
-            self.document[dei.name] = {'attrs' : dei.attrs , 'value' : dei.contents}
+        self.document = nested_dict()
+        for dei in self._xbrl.find_all(lambda tag : tag.prefix == 'dei' and 'contextRef' in tag.attrs):
+            self.document[dei.name][dei.attrs['contextRef']] = {'attrs' : dei.attrs , 'value' : dei.contents}
                 
         ##########################
         #Build the xbrl "gaap" fields - anything and everything reported as GAAP field
         #
-        self.gaap = {}
-        for gaap in self._xbrl.find_all(lambda tag : tag.prefix == 'us-gaap' and self._idsearch.search(tag.attrs['id']) is not None):
-            self.gaap[gaap.name] = {'attrs' : gaap.attrs , 'value' : gaap.contents}
-                
+        self.gaap = nested_dict()
+        for gaap in self._xbrl.find_all(lambda tag : tag.prefix == 'us-gaap' and 'contextRef' in tag.attrs):
+            self.gaap[gaap.name][gaap.attrs['contextRef']] = {'attrs' : gaap.attrs , 'value' : gaap.contents}
+            
         ##########################
         #Build the xbrl "special" fields - non standard gaap prefix (maybe stock ticker lowercase?)
         #
-        extra_prefix = self._xbrl.find(lambda tag : tag.prefix != 'us-gaap' and tag.prefix != 'dei'
-                                      and 'id' in tag.attrs 
-                                      and self._idsearch.search(tag.attrs['id']) is not None).prefix
+        
+        extra_prefix = self._xbrl.find(lambda tag : tag.prefix != 'us-gaap' and tag.prefix != 'dei' and 'contextRef' in tag.attrs)
 
-        self.special= {}
-        for special in self._xbrl.find_all(lambda tag : tag.prefix == extra_prefix and self._idsearch.search(tag.attrs['id']) is not None):
-            self.special[special.name] = {'attrs' : special.attrs , 'value' : special.contents}
+        self.special= nested_dict()
+
+        if extra_prefix is not None:
+            extra_prefix = extra_prefix.prefix
+            for special in self._xbrl.find_all(lambda tag : tag.prefix == extra_prefix and 'contextRef' in tag.attrs):
+                self.special[special.name][special.attrs['contextRef']] = {'attrs' : special.attrs , 'value' : special.contents}
 
 
         
